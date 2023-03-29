10f91ef6fcf0f4bd6e30a8dba91b7f09929c8efe
polashbiswas <30291056+polashbiswas@users.noreply.github.com>
2023-03-29 19:37:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
 function threeSum(arr, target) {
 // write your code here
-  
+	arr.sort(function(a,b) { return a-b});
+	let diff = MAX_VALUE;
+	let ans = 0;
+	for(int i = 0; i < arr.length; i++){
+		let first = arr[i];
+		let start = i+1;
+		let end = arr.length-1;
+		while(start < end){
+			if(first + arr[start] + arr[end] === target){
+				return target;
+			}
+			else if(Math.abs(first + arr[start] + arr[end] - target) < diff){
+				diff = Math.abs(first + arr[start] + arr[end] - target);
+				ans = first + arr[start] + arr[end];
+			}
+			if(first + arr[start] + arr[end] > target){
+				end--;
+			}else{
+				start++;
+			}
+		}
+	}
+  return ans;
 }
 
 module.exports = threeSum;
